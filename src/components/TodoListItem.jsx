749b1a11fa2e55d29457881f255e9601ac2e9797
Ryutaro0802 <ne200152g@gmail.com>
2018-12-02 23:52:46 +0900

    aphrodite導入した

--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import TodoListItemDetail from "./TodoListItemDetail.jsx";
+import { StyleSheet, css } from "aphrodite";
 
 export default class TodoListItem extends Component {
   constructor(props) {
@@ -81,7 +82,28 @@ export default class TodoListItem extends Component {
         </button>
 
         {this.props.isEditing && <TodoListItemDetail />}
+
+        <span className={css(styles.red)}>Red!</span>
       </li>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  red: {
+    backgroundColor: "red"
+  },
+  blue: {
+    backgroundColor: "blue"
+  },
+  hover: {
+    ":hover": {
+      backgroundColor: "red"
+    }
+  },
+  small: {
+    "@media (max-width: 600px)": {
+      backgroundColor: "red"
+    }
+  }
+});
