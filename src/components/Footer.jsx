7798a2ccec97c65978bd22a47548b03ef7345f98
Ryutaro0802 <ne200152g@gmail.com>
2019-03-06 01:12:50 +0900

    Footerにtag追加リンク

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,12 @@ export default class Footer extends Component {
   render() {
     return (
       <footer className={css(styles.footer)}>
-        <small>Todo Items: {this.props.todoLength}</small>
+        <small className={css(styles.tagLength)}>
+          Items: {this.props.todoLength}
+        </small>
+        <small className={css(styles.addTag)}>
+          <a href="#" className={css(styles.addTagAnc)}>Add tag +</a>
+        </small>
       </footer>
     );
   }
@@ -16,6 +21,13 @@ const styles = StyleSheet.create({
   footer: {
     color: cssVariables.colors.text,
     padding: "10px 0",
-    borderTop: `1px solid ${cssVariables.colors.border}`
+    borderTop: `1px solid ${cssVariables.colors.border}`,
+    display: "flex"
+  },
+  tagLength: {
+    flexGrow: 1
+  },
+  addTagAnc: {
+    color: 'inherit'
   }
 });
