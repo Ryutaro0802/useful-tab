e3a6ca9e177c0f154bcee2eb6a360bd1658fc769
Ryutaro0802 <ne200152g@gmail.com>
2019-03-06 01:40:56 +0900

    memoガジェット追加

---
+++ src/components/memo/Memo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Memo extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return <textarea name="" id="" cols="30" rows="10"></textarea>
+    }
+}
+
+export default Memo;
