fbf2dd912274340f0ae0309b7b2d06663272914c
Ryutaro0802 <ne200152g@gmail.com>
2019-03-06 02:11:54 +0900

    memoガジェットのvalue保存

--- src/components/memo/Memo.jsx
+++ src/components/memo/Memo.jsx
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import { StyleSheet, css } from "aphrodite";
+import {setItems, getItems} from '../../util/storage.js';
+
+const memoGadgetKey = 'useful-tab-memo';
+const defaultValue = JSON.parse(getItems(memoGadgetKey)) || '';
 
 class Memo extends Component {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+    this.state = { value: defaultValue };
+  }
 
-    render() {
-        return <textarea name="" id="" cols="30" rows="10"></textarea>
-    }
+  inputHandler = e => {
+    this.setState({
+        value: e.target.value
+    });
+    setItems(memoGadgetKey, JSON.stringify(this.state.value));
+  }
+
+  render() {
+    return (
+      <div>
+        <textarea
+            name=""
+            value={this.state.value}
+            cols="30"
+            rows="10"
+            onInput={ this.inputHandler }
+         />
+      </div>
+    );
+  }
 }
 
 export default Memo;
