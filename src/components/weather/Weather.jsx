7bb655fc9eb5191151e549c34ef1998019b4fef5
Ryutaro0802 <ne200152g@gmail.com>
2019-03-18 00:14:22 +0900

    geolocationを使えるか判定

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 
 export default class Weather extends Component {
+    componentDidMount() {
+        if (!'geolocation' in navigator) {
+            return;
+        }
+
+    }
   render() {
     return <div>aaa</div>;
   }
