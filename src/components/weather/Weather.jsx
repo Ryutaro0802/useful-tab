5f8d99676e32bd53116c86f2bf1fbd72c581a100
Ryutaro0802 <ne200152g@gmail.com>
2019-03-18 01:53:36 +0900

    ID追加

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 
 export default class Weather extends Component {
-    componentDidMount() {
-        if (!'geolocation' in navigator) {
-            return;
-        }
-
+  componentDidMount() {
+    const API_KEY = "059443ed516a4cc9d83a2e21ac0b645e";
+    const API_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast";
+    if (!"geolocation" in navigator) {
+      return;
     }
+  }
   render() {
     return <div>aaa</div>;
   }
