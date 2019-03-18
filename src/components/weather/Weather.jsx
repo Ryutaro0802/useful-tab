5e1075ef55027f45e4481ecc27bad5f3dcaeca98
Ryutaro0802 <ne200152g@gmail.com>
2019-03-19 00:43:49 +0900

    API call

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -4,6 +4,15 @@ export default class Weather extends Component {
   componentDidMount() {
     const API_KEY = "059443ed516a4cc9d83a2e21ac0b645e";
     const API_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast";
+    const CITY = "Tokyo";
+    const URL = `${API_BASE_URL}?q=${CITY},jp&units=metric&APPID=${API_KEY}`;
+    fetch(URL)
+      .then(response => {
+        return response.json();
+      })
+      .then(body => {
+        console.log(body);
+      });
     if (!"geolocation" in navigator) {
       return;
     }
