4c654ef90beae1856813091d0a4b15ed99b21499
Ryutaro0802 <ne200152g@gmail.com>
2019-03-19 02:01:34 +0900

    textarea design

--- src/components/memo/Memo.jsx
+++ src/components/memo/Memo.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { StyleSheet, css } from "aphrodite";
-import {setItems, getItems} from '../../util/storage.js';
+import { setItems, getItems } from "../../util/storage.js";
 
-const memoGadgetKey = 'useful-tab-memo';
-const defaultValue = JSON.parse(getItems(memoGadgetKey)) || '';
+const memoGadgetKey = "useful-tab-memo";
+const defaultValue = JSON.parse(getItems(memoGadgetKey)) || "";
 
 export default class Memo extends Component {
   constructor() {
@@ -13,23 +13,33 @@ export default class Memo extends Component {
 
   inputHandler = e => {
     this.setState({
-        value: e.target.value
+      value: e.target.value
     });
     setItems(memoGadgetKey, JSON.stringify(this.state.value));
-  }
+  };
 
   render() {
     return (
       <div>
         <textarea
-            className="aaa"
-            name=""
-            value={this.state.value}
-            cols="30"
-            rows="10"
-            onInput={ this.inputHandler }
-         />
+          className={css(styles.memo)}
+          name=""
+          value={this.state.value}
+          cols="30"
+          rows="10"
+          onInput={this.inputHandler}
+        />
       </div>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  memo: {
+    "appearance": "none",
+    "-webkit-appearance": "none",
+    "border": "1px solid #c4c4c4",
+    "padding": ".5em",
+    "cursor": "text"
+  }
+});
