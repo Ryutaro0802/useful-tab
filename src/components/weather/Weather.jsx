f8c7510a400ffc5c25610418954ac218d04c71c8
Ryutaro0802 <ne200152g@gmail.com>
2019-03-19 00:48:52 +0900

    axiosとdayjsを導入

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -1,18 +1,14 @@
 import React, { Component } from "react";
-
+import axios from 'axios';
 export default class Weather extends Component {
-  componentDidMount() {
+  async componentDidMount() {
     const API_KEY = "059443ed516a4cc9d83a2e21ac0b645e";
     const API_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast";
     const CITY = "Tokyo";
     const URL = `${API_BASE_URL}?q=${CITY},jp&units=metric&APPID=${API_KEY}`;
-    fetch(URL)
-      .then(response => {
-        return response.json();
-      })
-      .then(body => {
-        console.log(body);
-      });
+    const response = await axios.get(URL);
+    const weatherList = response.data.list;
+    console.log(weatherList);
     if (!"geolocation" in navigator) {
       return;
     }
